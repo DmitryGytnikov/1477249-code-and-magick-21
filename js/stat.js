5a630664346e8d5bad69258b523ce87eea860f38
DmitryGytnikov <gytnikovd@mail.ru>
2020-11-16 16:00:13 +0300

    Исправил частично замечания Александра

--- js/stat.js
+++ js/stat.js
@@ -1,45 +1,37 @@
 'use strict';
 
-const CLOUD_WIDTH = 420;
-const CLOUD_HEIGHT = 270;
-const CLOUD_X = 100;
-const CLOUD_Y = 10;
-const GAP = 20;
-const BAR_WIDTH = 40;
-const BAR_HEIGHT_MAX = 150;
-const BAR_GAP = 50;
-
-let renderCloud = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+const CLOUD = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  X: 100,
+  Y: 10
 };
 
-let getMaxElement = function (arr) {
-  let maxElement = arr[0];
+const GAP = 20;
+const BAR = {
+  WIDTH: 40,
+  HEIGHT_MAX: 150,
+  GAP: 50
+};
+const ColorBarYou = `rgba(255, 0, 0, 1)`;
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
-
-  return maxElement;
+const renderCloud = function (ctx, x, y, colorCloud, colorShadow) {
+  ctx.fillStyle = colorShadow;
+  ctx.fillRect(x + 0.5 * GAP, y + 0.5 * GAP, CLOUD.WIDTH, CLOUD.HEIGHT);
+  ctx.fillStyle = colorCloud;
+  ctx.fillRect(x, y, CLOUD.WIDTH, CLOUD.HEIGHT);
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  let maxTime = getMaxElement(times);
+
+  const maxTime = Math.max(...times);
 
   renderCloud(
       ctx,
-      CLOUD_X + 0.5 * GAP,
-      CLOUD_Y + 0.5 * GAP,
+      CLOUD.X,
+      CLOUD.Y,
+      `#fff`,
       `rgba(0, 0, 0, 0.7)`
-  );
-  renderCloud(
-      ctx,
-      CLOUD_X,
-      CLOUD_Y,
-      `#fff`
   );
 
   ctx.fillStyle = `#000`;
@@ -47,45 +39,48 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillText(
       `Ура вы победили!`,
-      CLOUD_X + GAP,
-      CLOUD_Y + 1.5 * GAP
+      CLOUD.X + GAP,
+      CLOUD.Y + 1.5 * GAP
   );
   ctx.fillText(
       `Список результатов:`,
-      CLOUD_X + GAP,
-      CLOUD_Y + 2.5 * GAP
+      CLOUD.X + GAP,
+      CLOUD.Y + 2.5 * GAP
   );
 
   for (let i = 0; i < names.length; i++) {
-    let calcPlayerX = CLOUD_X + 2 * GAP + (BAR_WIDTH + BAR_GAP) * i;
-    let calcBarHeight = (BAR_HEIGHT_MAX * times[i]) / maxTime;
-    //   maxTime     times[i]
-    // ----------- = --------
-    //  BAR_HEIGHT        X
-    //  X = (BAR_HEIGHT * times[i]) / maxTime
 
-    let colorBar = `hsl(240, ` + Math.random() * 100 + `%` + `, 50%)`;
+    const calcPlayerX = CLOUD.X + 2 * GAP + (BAR.WIDTH + BAR.GAP) * i;
+    const calcBarHeight = (BAR.HEIGHT_MAX * times[i]) / maxTime;
+    const getColorBar = `hsl(240, ` + Math.random() * 100 + `%` + `, 50%)`;
 
     ctx.fillStyle = `#000`;
     ctx.fillText(
         names[i],
         calcPlayerX,
-        CLOUD_Y + CLOUD_HEIGHT - GAP
+        CLOUD.Y + CLOUD.HEIGHT - GAP
     );
     ctx.fillText(
         Math.round(times[i]),
         calcPlayerX,
-        CLOUD_Y + CLOUD_HEIGHT - 2 * GAP - calcBarHeight - 0.5 * GAP
+        CLOUD.Y + CLOUD.HEIGHT - 2 * GAP - calcBarHeight - 0.5 * GAP
     );
 
-    ctx.fillStyle = (names[i] === `Вы`) ? `rgba(255, 0, 0, 1)` : colorBar;
-    // Синий цвет: rgba(0, 0, 255, 1) или hsl(240, 100%, 50%)
+    ctx.fillStyle = (names[i] === `Вы`) ? ColorBarYou : getColorBar;
 
     ctx.fillRect(
         calcPlayerX,
-        CLOUD_Y + CLOUD_HEIGHT - 2 * GAP,
-        BAR_WIDTH,
+        CLOUD.Y + CLOUD.HEIGHT - 2 * GAP,
+        BAR.WIDTH,
         -calcBarHeight
     );
   }
 };
+
+// Я пока не представляю, как исправить последние два замечания
+// ("отрисовку колонок гистограммы лучше вынести в отдельную функцию" и
+//  "изначально в ТЗ текст идёт одной строкой. Хорошо бы создать метод,
+//  который сам сможет делить текст на строки при наличии разделителей").
+// Пожалуйста, сделай мердж этого пулреквеста (пусть и с ошибками): чтобы
+//  я смог забрать изменения из репозитория Академии в свою локальную
+//  ветку master, и доделать проект уже в своей локальной ветке master.

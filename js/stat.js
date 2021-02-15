e2a3591dac7bd8b02e69397fb0135b8ef437ee2d
DmitryGytnikov <gytnikovd@mail.ru>
2021-02-15 19:05:47 +0300

    Небольшие изменения

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CLOUD = {
+const Cloud = {
   WIDTH: 420,
   HEIGHT: 270,
   X: 100,
@@ -8,18 +8,18 @@ const CLOUD = {
 };
 
 const GAP = 20;
-const BAR = {
+const Bar = {
   WIDTH: 40,
   HEIGHT_MAX: 150,
   GAP: 50
 };
-const ColorBarYou = `rgba(255, 0, 0, 1)`;
+const COLOR_BAR_YOU = `rgba(255, 0, 0, 1)`;
 
 const renderCloud = function (ctx, x, y, colorCloud, colorShadow) {
   ctx.fillStyle = colorShadow;
-  ctx.fillRect(x + 0.5 * GAP, y + 0.5 * GAP, CLOUD.WIDTH, CLOUD.HEIGHT);
+  ctx.fillRect(x + 0.5 * GAP, y + 0.5 * GAP, Cloud.WIDTH, Cloud.HEIGHT);
   ctx.fillStyle = colorCloud;
-  ctx.fillRect(x, y, CLOUD.WIDTH, CLOUD.HEIGHT);
+  ctx.fillRect(x, y, Cloud.WIDTH, Cloud.HEIGHT);
 };
 
 window.renderStatistics = function (ctx, names, times) {
@@ -28,8 +28,8 @@ window.renderStatistics = function (ctx, names, times) {
 
   renderCloud(
       ctx,
-      CLOUD.X,
-      CLOUD.Y,
+      Cloud.X,
+      Cloud.Y,
       `#fff`,
       `rgba(0, 0, 0, 0.7)`
   );
@@ -39,39 +39,39 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillText(
       `Ура вы победили!`,
-      CLOUD.X + GAP,
-      CLOUD.Y + 1.5 * GAP
+      Cloud.X + GAP,
+      Cloud.Y + 1.5 * GAP
   );
   ctx.fillText(
       `Список результатов:`,
-      CLOUD.X + GAP,
-      CLOUD.Y + 2.5 * GAP
+      Cloud.X + GAP,
+      Cloud.Y + 2.5 * GAP
   );
 
   for (let i = 0; i < names.length; i++) {
 
-    const calcPlayerX = CLOUD.X + 2 * GAP + (BAR.WIDTH + BAR.GAP) * i;
-    const calcBarHeight = (BAR.HEIGHT_MAX * times[i]) / maxTime;
+    const calcPlayerX = Cloud.X + 2 * GAP + (Bar.WIDTH + Bar.GAP) * i;
+    const calcBarHeight = (Bar.HEIGHT_MAX * times[i]) / maxTime;
     const getColorBar = `hsl(240, ` + Math.random() * 100 + `%` + `, 50%)`;
 
     ctx.fillStyle = `#000`;
     ctx.fillText(
         names[i],
         calcPlayerX,
-        CLOUD.Y + CLOUD.HEIGHT - GAP
+        Cloud.Y + Cloud.HEIGHT - GAP
     );
     ctx.fillText(
         Math.round(times[i]),
         calcPlayerX,
-        CLOUD.Y + CLOUD.HEIGHT - 2 * GAP - calcBarHeight - 0.5 * GAP
+        Cloud.Y + Cloud.HEIGHT - 2 * GAP - calcBarHeight - 0.5 * GAP
     );
 
-    ctx.fillStyle = (names[i] === `Вы`) ? ColorBarYou : getColorBar;
+    ctx.fillStyle = (names[i] === `Вы`) ? COLOR_BAR_YOU : getColorBar;
 
     ctx.fillRect(
         calcPlayerX,
-        CLOUD.Y + CLOUD.HEIGHT - 2 * GAP,
-        BAR.WIDTH,
+        Cloud.Y + Cloud.HEIGHT - 2 * GAP,
+        Bar.WIDTH,
         -calcBarHeight
     );
   }
